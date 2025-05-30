9c7f11d81e32771ce4d6471464eef8021022cf91
DAveTheHunter <dawetbiniyame@gmail.com>
2025-05-30 22:39:13 +0300

    Starting it

---
+++ server.c
@@ -0,0 +1,39 @@
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+#define port 8000
+#define buffer_size 1024
+#define thread_count 4
+#define size 256
+#define max_client 100
+
+//the task structure 
+typedef struct{
+  void (*function)(void*); 
+  void* arg;
+}Task;
+
+//shared clients list 
+int clients[max_client];
+int clients_count = 0;
+pthread_mutex_t client_lock;
+
+Task tasks[size];
+int task_count =  0;
+int head = 0,tail = 0;
+pthread_mutex_t lock;
+pthread_cond_t cond;
+bool shutdowns = false;
+
+int main(int argc, char *argv[]){
+
+  return EXIT_SUCCESS;
+}
