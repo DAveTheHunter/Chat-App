a93c0cdc087b704cb3ea105e13e899ccff7f1ff4
DAveTheHunter <dawetbiniyame@gmail.com>
2025-05-30 22:48:05 +0300

    Implmented sending message future

--- server.c
+++ server.c
@@ -1,13 +1,13 @@
 #include <arpa/inet.h>
-#include <pthread.h>
+#include <errno.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
 #include <sys/socket.h>
-#include <errno.h>
+#include <unistd.h>
 
 #define port 8000
 #define buffer_size 1024
@@ -15,26 +15,26 @@
 #define size 256
 #define max_client 100
 
-//the task structure 
-typedef struct{
-  void (*function)(void*); 
-  void* arg;
-}Task;
+// the task structure
+typedef struct {
+  void (*function)(void *);
+  void *arg;
+} Task;
 
-//shared clients list 
+// shared clients list
 int clients[max_client];
 int clients_count = 0;
 pthread_mutex_t client_lock;
 
 Task tasks[size];
-int task_count =  0;
-int head = 0,tail = 0;
+int task_count = 0;
+int head = 0, tail = 0;
 pthread_mutex_t lock;
 pthread_cond_t cond;
 bool shutdowns = false;
 
-//add client 
-bool add_client(int client_fd){
+// add client
+bool add_client(int client_fd) {
   pthread_mutex_lock(&client_lock);
   if (clients_count > max_client) {
     pthread_mutex_unlock(&client_lock);
@@ -45,20 +45,30 @@ bool add_client(int client_fd){
   return true;
 }
 
-void remove_client(int client_fd){
+void remove_client(int client_fd) {
   pthread_mutex_lock(&client_lock);
-  for (int i = 0;i < clients_count;i++) {
+  for (int i = 0; i < clients_count; i++) {
     if (clients[i] == client_fd) {
       clients[i] = clients[--clients_count];
       break;
-    }  
+    }
   }
   pthread_mutex_unlock(&client_lock);
 }
 
-
-
-int main(int argc, char *argv[]){
-
-  return EXIT_SUCCESS;
+void brodcast_message(const char *message, int sender_fd) {
+  pthread_mutex_lock(&client_lock);
+  for (int i = 0; i < clients_count; i++) {
+    if (clients[i] != sender_fd) {
+      if (write(clients[i], message, strlen(message)) < 0) {
+        printf("Error sending message to client %d\n",clients[i]);
+        close(clients[i]);
+        clients[i] = clients[--clients_count];
+        i--;
+      }
+    }
+  }
+  pthread_mutex_unlock(&client_lock);
 }
+
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
