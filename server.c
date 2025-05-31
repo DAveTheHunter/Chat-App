0278d2c600b67952bd8447acbf8cd33366ff2d3c
DAveTheHunter <dawetbiniyame@gmail.com>
2025-05-31 12:41:41 +0300

    Done server!!!

--- server.c
+++ server.c
@@ -61,7 +61,7 @@ void brodcast_message(const char *message, int sender_fd) {
   for (int i = 0; i < clients_count; i++) {
     if (clients[i] != sender_fd) {
       if (write(clients[i], message, strlen(message)) < 0) {
-        printf("Error sending message to client %d\n",clients[i]);
+        printf("Error sending message to client %d\n", clients[i]);
         close(clients[i]);
         clients[i] = clients[--clients_count];
         i--;
@@ -71,14 +71,14 @@ void brodcast_message(const char *message, int sender_fd) {
   pthread_mutex_unlock(&client_lock);
 }
 
-void* worker(void* arg){
+void *worker(void *arg) {
   while (1) {
     pthread_mutex_lock(&lock);
     while (task_count == 0 && !shutdowns) {
-      pthread_cond_wait(&cond,&lock);
+      pthread_cond_wait(&cond, &lock);
     }
     if (shutdowns && task_count == 0) {
-       pthread_mutex_unlock(&lock);
+      pthread_mutex_unlock(&lock);
       break;
     }
     Task task = tasks[head];
@@ -86,11 +86,11 @@ void* worker(void* arg){
     task_count--;
     pthread_mutex_unlock(&lock);
     task.function(task.arg);
-  }  
+  }
   return NULL;
 }
 
-void submit_task(void(*function)(void*),void* arg){
+void submit_task(void (*function)(void *), void *arg) {
   pthread_mutex_lock(&lock);
   if (task_count >= size) {
     printf("maximum capacity\n");
@@ -99,93 +99,95 @@ void submit_task(void(*function)(void*),void* arg){
     return;
   }
   tasks[tail].function = function;
-  tasks[tail].arg= arg;
+  tasks[tail].arg = arg;
   tail = (tail + 1) % size;
   task_count++;
   pthread_cond_signal(&cond);
   pthread_mutex_unlock(&lock);
 }
 
-void shutdown_pool(pthread_t threads[]){
+void shutdown_pool(pthread_t threads[]) {
   pthread_mutex_lock(&lock);
   shutdowns = true;
   pthread_cond_broadcast(&cond);
   pthread_mutex_unlock(&lock);
-  for (int i = 0;i < thread_count;i++) {
+  for (int i = 0; i < thread_count; i++) {
     pthread_join(threads[i], NULL);
   }
-  //close all the client sockets 
+  // close all the client sockets
   pthread_mutex_lock(&lock);
-  for(int i = 0;i < clients_count;i++){
+  for (int i = 0; i < clients_count; i++) {
     close(clients[i]);
   }
   clients_count = 0;
   pthread_mutex_unlock(&lock);
 }
 
-void handle_client(void *arg){
-  int* client_fd_prt = (int*)arg;
+void handle_client(void *arg) {
+  int *client_fd_prt = (int *)arg;
   int client_fd = *client_fd_prt;
   free(client_fd_prt);
 
   char buffer[buffer_size];
-  int bytes_read = read(client_fd, buffer,buffer_size - 1);
+  int bytes_read = read(client_fd, buffer, buffer_size - 1);
   if (bytes_read <= 0) {
-    printf("Client %d disconnected\n",client_fd);
+    printf("Client %d disconnected\n", client_fd);
     remove_client(client_fd);
     close(client_fd);
     return;
-  }   
-   buffer[bytes_read] = '\0';
-  printf("Thread %ld reacived from client %d: %s\n",pthread_self(), client_fd,buffer);
-   brodcast_message(buffer,client_fd);
+  }
+  buffer[bytes_read] = '\0';
+  printf("Thread %ld reacived from client %d: %s\n", pthread_self(), client_fd,
+         buffer);
+  brodcast_message(buffer, client_fd);
 }
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[]) {
   pthread_t thread[thread_count];
   int server_fd;
   struct sockaddr_in server_addr;
-  //initialize the mutex and cond
-  pthread_mutex_init(&lock,NULL);
-  pthread_cond_init(&cond,NULL);
+  // initialize the mutex and cond
+  pthread_mutex_init(&lock, NULL);
+  pthread_cond_init(&cond, NULL);
 
-  //create a worker thread
-  for (int i = 0;i < thread_count;i++) {
-    pthread_create(&thread[i],NULL,worker,NULL);
+  // create a worker thread
+  for (int i = 0; i < thread_count; i++) {
+    pthread_create(&thread[i], NULL, worker, NULL);
   }
-  //create a server socket 
-  server_fd = socket(AF_INET,SOCK_STREAM,0);
+  // create a server socket
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
     printf("Socker formation failed\n");
     shutdown_pool(thread);
     pthread_mutex_destroy(&client_lock);
     pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&cond);
-    return EXIT_FAILURE; 
+    return EXIT_FAILURE;
   }
-  //set options
+  // set options
   int opt = 1;
-  if (setsockopt(server_fd, SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
     printf("Error setting options\n");
     close(server_fd);
     shutdown_pool(thread);
     pthread_mutex_destroy(&client_lock);
     pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&cond);
-    return EXIT_FAILURE; 
+    return EXIT_FAILURE;
   }
-  //Bind and listen
+  // Bind and listen
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(port);
-  if (bind(server_fd,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0) {
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
+      0) {
     printf("Bind was not succesful\n");
     close(server_fd);
     shutdown_pool(thread);
     pthread_mutex_destroy(&client_lock);
     pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&cond);
-    return EXIT_FAILURE; 
+    return EXIT_FAILURE;
   }
   if (listen(server_fd, 10) < 0) {
     printf("Linsten unsuccesful\n");
@@ -194,8 +196,41 @@ int main(int argc, char *argv[]) {
     pthread_mutex_destroy(&client_lock);
     pthread_mutex_destroy(&lock);
     pthread_cond_destroy(&cond);
-    return EXIT_FAILURE; 
+    return EXIT_FAILURE;
   }
-  printf("⚡server listening at port %d\n",port);
-  return EXIT_SUCCESS; 
+  printf("⚡server listening at port %d\n", port);
+  // accept clients
+  while (!shutdowns) {
+    struct sockaddr_in client_addr;
+    socklen_t client_size = sizeof(client_addr);
+    int client_fd =
+        accept(server_fd, (struct sockaddr *)&client_addr, &client_size);
+    if (client_fd < 0) {
+      if (errno == EINTR) {
+        continue;
+      }
+      printf("accept failed\n");
+      break;
+    }
+    if (!add_client(client_fd)) {
+      printf("Max clients reached\n");
+      close(client_fd);
+      continue;
+    }
+    int *client_fd_prt = malloc(sizeof(int));
+    if (!client_fd_prt) {
+      printf("Allocation failed\n");
+      remove_client(client_fd);
+      close(client_fd);
+      continue;
+    }
+    *client_fd_prt = client_fd;
+    submit_task(handle_client, client_fd_prt);
+  }
+  close(server_fd);
+  shutdown_pool(thread);
+  pthread_mutex_destroy(&client_lock);
+  pthread_mutex_destroy(&lock);
+  pthread_cond_destroy(&cond);
+  return EXIT_SUCCESS;
 }
